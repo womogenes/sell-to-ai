0510d778d22bf220817e4fde9b7d2b7e65f1a4a7
William Y. Feng <womogenes2@gmail.com>
2024-10-05 13:55:10 -0400

    Make landing page

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Sell to AI - Marketing Party Game",
+  description: "Compete with your friends to come up with the most convincing ",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" href="https://fav.farm/🏪" />
+      </head>
+      <body className={`${geistSans.className}`}>{children}</body>
+    </html>
+  );
+}
